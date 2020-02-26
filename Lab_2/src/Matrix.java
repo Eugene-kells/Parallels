e00d736a56347d9099de643756038a0dab4aaff3
Yevhenii Pivovarenko <yevhenii_pivovarenko@external.mckinsey.com>
2020-02-26 22:21:24 +0200

    Added matrix representation and simple matrixes files

---
+++ Lab_2/src/Matrix.java
@@ -0,0 +1,125 @@
+//import com.sun.tools.javac.util.List;
+//
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.lang.reflect.Array;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class Matrix {
+//    private ArrayList<Float> data;
+//    private ArrayList<Integer> factors;
+//    private ArrayList<Integer> sizes;
+//
+//    public Matrix(String filePath) throws Exception {
+//        data = new ArrayList<>();
+//        try {
+//            Scanner input = new Scanner(new File(filePath));
+//            int previous_row_size = -1;
+//            int num_of_rows = 0;
+//            while (input.hasNextLine()) {
+//                Scanner colReader = new Scanner(input.nextLine());
+//                ArrayList<Float> col = new ArrayList<>();
+//                while (colReader.hasNextInt()) {
+//                    col.add(colReader.nextFloat());
+//                }
+//                if (previous_row_size >= 0 && previous_row_size != col.size())
+//                    throw new Exception("All rows should have the same number of items");
+//                previous_row_size = col.size();
+//                num_of_rows++;
+//                data.addAll(col);
+//            }
+//            sizes = new ArrayList<>(List.of(num_of_rows, previous_row_size));
+//            factors = new ArrayList<>(List.of(previous_row_size, 1));
+//        } catch (FileNotFoundException exc) {
+//            System.out.println("No file " + filePath);
+//        }
+//    }
+//
+//    public Matrix(Integer... dim_sizes) {
+//        checkDimensions(dim_sizes);
+//        factors = calculateFactors(dim_sizes);
+//        sizes = new ArrayList<Integer>(Arrays.asList(dim_sizes));
+//    }
+//
+//    private ArrayList<Integer> calculateFactors(Integer... indexes) {
+//
+//    }
+//
+//    private void checkDimensions(Integer... indexes) {
+//        assert indexes.length > 0;
+//        for (int index: indexes)
+//            assert index > 0;
+//    }
+//
+//    private void checkIndexes(Integer... indexes) {
+//        assert indexes.length == sizes.size();
+//        for (int i = 0; i < indexes.length; i++)
+//            assert 0 <= indexes[i] && indexes[i] < sizes.get(i);
+//    }
+//
+//    private int calculateIndex(Integer... indexes) {
+//        checkIndexes();
+//        int index_sum = 0;
+//        for (int i = 0; i < indexes.length; i++) {
+//            index_sum += indexes[i] * factors.get(i);
+//        }
+//        return index_sum;
+//    }
+//
+//    public Float get(Integer... indexes) {
+//        int index = calculateIndex(indexes);
+//        return data.get(index);
+//    }
+//}
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Matrix {
+    private ArrayList<ArrayList<Integer>> data;
+
+    public Matrix(String filePath) throws Exception {
+        data = new ArrayList<>();
+        File file = new File(filePath);
+        Scanner input = new Scanner(file);
+        int previous_row_size = -1;
+        while (input.hasNextLine()) {
+            Scanner colReader = new Scanner(input.nextLine());
+            ArrayList<Integer> row = new ArrayList<>();
+            while (colReader.hasNextInt()) {
+                row.add(colReader.nextInt());
+            }
+            if (previous_row_size >= 0 && previous_row_size != row.size())
+                throw new Exception("All rows should have the same number of items");
+            previous_row_size = row.size();
+            data.add(row);
+        }
+    }
+
+    public Matrix(int x, int y) {
+        assert x > 0 && y > 0;
+        data = new ArrayList<>();
+        for (int i = 0; i < x; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < x; j++) {
+                row.add(0);
+            }
+            data.add(row);
+        }
+    }
+
+    public Integer get(int x, int y) {
+        return data.get(x).get(y);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (ArrayList<Integer> row: data)
+            result.append(row);
+        return result.toString();
+    }
+}
---
+++ Lab_2/src/MatrixMultiplication.java
@@ -0,0 +1,6 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) throws Exception {
+        Matrix matrix_1 = new Matrix("matrixes/matrix_1.txt");
+        Matrix matrix_2 = new Matrix("matrixes/matrix_2.txt");
+    }
+}
